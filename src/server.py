5b4755d86de33077b6b80d55e470d7030eb3369f
brums <korkina.vo@yandex.ru>
2023-03-05 16:01:13 +0700

    Подключение клиента и получение сообщения

--- src/server.py
+++ src/server.py
@@ -40,12 +40,37 @@ class Server:
         except Exception as e:
             print("Ошибка при запуске сервера")
 
+def accept_client(self, client_reader: asyncio.StreamReader, 
+                  client_writer: asyncio.StreamWriter):
+    """Callback-функция, запускаемая при подключении клиента
+    :param client_reader: StreamReader
+    :type client_reader: asyncio.StreamReader
+    :param client_writer: StreamWriter
+    :type client_writer: asyncio.StreamWriter
+    """
+    task = asyncio.create_task(self.handle_client(client_reader, client_writer))
+    client_ip = client_writer.get_extra_info('peername')[0]
+    client_port = client_writer.get_extra_info('peername')[1]
+    print(f"Подключен новый клиент: {client_ip}:{client_port}")
+ 
+async def handle_client(self, client_reader: asyncio.StreamReader):
+    """ Получение сообщения клиента
+    :param client_reader: StreamReader
+    :type client_reader: asyncio.StreamReader
+    """
+    # пока простое чтение строки
+    client_message = str((await client_reader.read(255)).decode('utf8'))
+    print(client_message)
+
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        sys.exit(f"Usage: {sys.argv[0]} HOST_IP PORT")
+    argv_len = 3
+    if len(sys.argv) < argv_len:
+        sys.exit(
+            f"Неверный хост или порт."
+            f"Введите строку типа: {sys.argv[0]} HOST_IP PORT"
+        )
 
     loop = asyncio.get_event_loop()
     server = Server(sys.argv[1], sys.argv[2], loop)
     server.start_server()
-    
