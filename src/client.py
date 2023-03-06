717181a9a5751ae657d161a781fc5388a13c3f20
brums <korkina.vo@yandex.ru>
2023-03-06 16:38:06 +0700

    Добавление логирования для клиента

--- src/client.py
+++ src/client.py
@@ -1,6 +1,8 @@
 import asyncio
 import sys
+import logging
 from aioconsole import ainput
+from logger import initialize_logger
 
 
 class Client:
@@ -9,6 +11,7 @@ class Client:
         self.__server_port: int = server_port
         self.__reader: asyncio.StreamReader = None
         self.__writer: asyncio.StreamWriter = None
+        self.__logger: logging.Logger = initialize_logger('client')
 
     @property
     def server_ip(self):
@@ -25,6 +28,10 @@ class Client:
     @property
     def writer(self):
         return self.__writer
+    
+    @property
+    def logger(self):
+        return self.__logger
 
     async def connect_to_server(self):
         """Подключение клиента к серверу"""
@@ -36,13 +43,13 @@ class Client:
                 self.client_input()
             )
         except Exception as e:
-            print("Ошибка в подключении клиента")
-
+            self.logger.error("Ошибка при подключении к серверу")
 
     async def receive_messages(self):
         server_message: str = None
         while server_message != 'exit':
             server_message = await self.get_server_message()
+            self.logger.info(f"Принято сообщение от сервера: {server_message}")
             print(f"{server_message}")
 
 
@@ -51,7 +58,10 @@ class Client:
 
     async def client_input(self):
         client_message: str = None
-        client_message = await ainput("Enter")
+        while True:
+            client_message = await ainput("Enter")
+            if all(ch in '0123456789 +-*/' for ch in client_message):
+                break
         self.writer.write(client_message.encode('utf8'))
         await self.writer.drain()
 
