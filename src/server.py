70738bbc97f3da57324033366ace686870a5cbc3
brums <korkina.vo@yandex.ru>
2023-03-06 09:55:08 +0700

    Изменена функция приема сообщения. Добавлено вычисление выражения.

--- src/server.py
+++ src/server.py
@@ -1,6 +1,9 @@
 """Модуль сервера"""
 import asyncio
 import sys
+import numexpr as ne
+
+from client import Client
 
 
 class Server:
@@ -40,32 +43,44 @@ class Server:
         except Exception as e:
             print("Ошибка при запуске сервера")
 
-def accept_client(self, client_reader: asyncio.StreamReader, 
-                  client_writer: asyncio.StreamWriter):
-    """Callback-функция, запускаемая при подключении клиента
-    :param client_reader: StreamReader
-    :type client_reader: asyncio.StreamReader
-    :param client_writer: StreamWriter
-    :type client_writer: asyncio.StreamWriter
-    """
-    task = asyncio.create_task(self.handle_client(client_reader, client_writer))
-    client_ip = client_writer.get_extra_info('peername')[0]
-    client_port = client_writer.get_extra_info('peername')[1]
-    print(f"Подключен новый клиент: {client_ip}:{client_port}")
- 
-async def handle_client(self, client_reader: asyncio.StreamReader):
-    """ Получение сообщения клиента
-    :param client_reader: StreamReader
-    :type client_reader: asyncio.StreamReader
-    """
-    # пока простое чтение строки
-    client_message = str((await client_reader.read(255)).decode('utf8'))
-    print(client_message)
+    def accept_client(self, client_reader: asyncio.StreamReader, 
+                    client_writer: asyncio.StreamWriter):
+        """Callback-функция, запускаемая при подключении клиента
+        :param client_reader: StreamReader
+        :type client_reader: asyncio.StreamReader
+        :param client_writer: StreamWriter
+        :type client_writer: asyncio.StreamWriter
+        """
+        client = Client(client_reader, client_writer)
+        task = asyncio.create_task(self.receiving_and_calc(client))
+        client_ip = client_writer.get_extra_info('peername')[0]
+        client_port = client_writer.get_extra_info('peername')[1]
+        print(f"Подключен новый клиент: {client_ip}:{client_port}")
+    
+    async def receiving_and_calc(self, client: Client):
+        """Прием сообщения пользователя, возвращение результатов вычисления
+        Возвращает результат в случае верного ввода, иначе сообщение об ошибке
+        :param client: клиент, посылающий сообщение
+        :type client: Client
+        """
+        while True:
+            client_message = await client.send_message()
+            if client_message == "exit":
+                break
+            try:
+                result_eval = str(float(ne.evaluate(client_message)))
+            except ValueError:
+                result_eval = "Вычисление невозможно."\
+                    "Не является простым математическим выражением."
+                
+            client.writer.write(result_eval)
+            await client.writer.drain()
+
+        print("Соединение с клиентом прервано")
 
 
 if __name__ == '__main__':
-    argv_len = 3
-    if len(sys.argv) < argv_len:
+    if len(sys.argv) < 3:
         sys.exit(
             f"Неверный хост или порт."
             f"Введите строку типа: {sys.argv[0]} HOST_IP PORT"
