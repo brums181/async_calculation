487d8ff64ce81f97628b3e6f79c9e5069b543754
brums <korkina.vo@yandex.ru>
2023-03-06 19:11:48 +0700

    Добавлена документация и вывод о невозможности вычисления

--- src/client.py
+++ src/client.py
@@ -1,3 +1,4 @@
+"""Модуль клиента"""
 import asyncio
 import sys
 import logging
@@ -7,9 +8,21 @@ from logger import initialize_logger
 
 
 class Client:
+    """Класс клиента
+    :param server_ip: ip сервера
+    :type server_ip: str
+    :param server_port: порт сервера
+    :type server_port: int
+    :param reader: StreamReader
+    :type reader: asyncio.StreamReader
+    :param writer: StreamWriter
+    :type writer: asyncio.StreamWriter
+    :param logger: логгер
+    :type logger: logging.Logger
+    """
     def __init__(self, server_ip: str, server_port: int):
-        self.__server_ip: str = server_ip
-        self.__server_port: int = server_port
+        self.__server_ip = server_ip
+        self.__server_port = server_port
         self.__reader: asyncio.StreamReader = None
         self.__writer: asyncio.StreamWriter = None
         self.__logger: logging.Logger = initialize_logger('client')
@@ -47,24 +60,35 @@ class Client:
             self.logger.error("Ошибка при подключении к серверу")
 
     async def receive_messages(self):
+        """Принятие сообщения от сервера"""
         server_message: dict = {'result': None}
         server_message = await self.get_server_message()
-        result_str = f"Принято сообщение от сервера. "\
-                    f"Выражение {server_message['expression']} = "\
-                    f"{server_message['result']}"
+        if server_message['result']:
+            result_str = f"Принято сообщение от сервера. "\
+                        f"Выражение {server_message['expression']} = "\
+                        f"{server_message['result']}"
+        else:
+            result_str = "Не является простым математическим выражением"
         print(result_str)
         self.logger.info(result_str)
 
-
     async def get_server_message(self):
+        """Чтение сообщения и его декодирование"""
         return json.loads((await self.reader.read(255)).decode('utf8'))   
 
     async def client_input(self):
+        """Клиентский ввод"""
         client_message: str = None
-        while True:
-            client_message = await ainput("Enter")
-            if all(ch in '0123456789 +-*/' for ch in client_message):
-                break
+        wrong_input = True
+        while wrong_input:
+            client_message = await ainput(
+                "Введите простое математическое выражение: \n"
+            )
+            if all(ch in '0123456789 +-*/()' for ch in client_message):
+                wrong_input = False
+            else: 
+                print("Ошибка ввода. ")
+                print("Выражение не должно содержать буквы или знаки кроме +-*/")
         self.writer.write(client_message.encode('utf8'))
         await self.writer.drain()
 
